8171303891ee0a45335e863a9ee6a6cbba19b9ae
Garry Sharp <>
2025-06-09 13:09:32 +0400

    🚚 Restored main.go back to its previous generate statement

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
-//go:generate protoc --proto_path=/usr/local/include/include -I/usr/local/include/include -I./proto --go_opt=paths=source_relative --go_out=./types proto/constraint.proto proto/rule.proto proto/parameter_constraint.proto proto/resource.proto proto/policy.proto proto/recipe_specification.proto proto/scheduling.proto
-
+//go:generate protoc -I./proto --go_opt=paths=source_relative --go_out=./types proto/constraint.proto proto/rule.proto proto/parameter_constraint.proto proto/resource.proto proto/policy.proto proto/recipe_specification.proto proto/scheduling.proto
 func main() {
 
 }
