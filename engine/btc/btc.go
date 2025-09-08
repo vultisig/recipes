4477ccc31dbebe047eb7a5df88942f1ff961e4be
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-09-08 22:14:40 +0300

    validation update

--- engine/btc/btc.go
+++ engine/btc/btc.go
@@ -26,7 +26,7 @@ func (b *Btc) Evaluate(rule *types.Rule, txBytes []byte) error {
 	if rule.GetEffect().String() != types.Effect_EFFECT_ALLOW.String() {
 		return fmt.Errorf("only allow rules supported, got: %s", rule.GetEffect().String())
 	}
-	if rule.GetTarget() != nil {
+	if rule.GetTarget().GetTargetType() != types.TargetType_TARGET_TYPE_UNSPECIFIED {
 		return fmt.Errorf("target must be nil for BTC, got: %s", rule.GetTarget().String())
 	}
 
