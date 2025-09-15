abf549f46b51b0abd4d2d780b41d133ff734e624
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-09-15 19:49:34 +0300

    fix CopyProto

--- internal/conv/conv.go
+++ internal/conv/conv.go
@@ -1,31 +1,14 @@
 package conv
 
 import (
-	"fmt"
-	"reflect"
+	"errors"
 
-	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
 func CopyProto[T proto.Message](in T) (T, error) {
-	b, err := protojson.Marshal(in)
-	if err != nil {
-		return *new(T), fmt.Errorf("failed to marshal proto: %w", err)
+	if in == nil {
+		return *new(T), errors.New("nil input")
 	}
-
-	out := *new(T)
-	v := reflect.ValueOf(out)
-	if v.Kind() == reflect.Ptr && v.IsNil() {
-		// For pointer types, we need to create a new instance
-		out = proto.Clone(in).(T)
-		proto.Reset(out)
-	}
-
-	err = protojson.Unmarshal(b, out)
-	if err != nil {
-		return *new(T), fmt.Errorf("failed to unmarshal proto: %w", err)
-	}
-
-	return out, nil
+	return proto.Clone(in).(T), nil
 }
