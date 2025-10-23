ce193b2b77d28486502dfd003a8a488c05fc6403
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-10-23 22:30:43 +0300

    asset schema

--- jsonschema/asset.go
+++ jsonschema/asset.go
@@ -10,28 +10,28 @@ func (a Asset) Name() string {
 	return "asset"
 }
 
-func (a Asset) Schema() map[string]interface{} {
-	return map[string]interface{}{
+func (a Asset) Schema() map[string]any {
+	return map[string]any{
 		"$schema":     "https://json-schema.org/draft/2020-12/schema",
 		"$id":         id("asset"),
 		"title":       "Asset",
 		"description": "An asset schema representing a blockchain asset with token, chain, and address information",
 		"type":        "object",
-		"properties": map[string]interface{}{
-			"token": map[string]interface{}{
+		"properties": map[string]any{
+			"token": map[string]any{
 				"type":        "string",
 				"description": "Smart-contract address. Empty for native asset",
 			},
-			"chain": map[string]interface{}{
+			"chain": map[string]any{
 				"type":        "string",
 				"description": "Blockchain network identifier as common.Chain string representation",
 			},
-			"address": map[string]interface{}{
+			"address": map[string]any{
 				"type":        "string",
 				"description": "User address on the blockchain",
 			},
 		},
-		"required":             []string{"chain", "address"},
+		"required":             []any{"chain", "address"},
 		"additionalProperties": false,
 	}
 }
