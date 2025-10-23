ee48a6da5f4644b1615a13f8c8990da53c6ade83
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-10-23 21:30:48 +0300

    asset schema

---
+++ jsonschema/types.go
@@ -0,0 +1,27 @@
+package jsonschema
+
+func Asset() map[string]interface{} {
+	return map[string]interface{}{
+		"$schema":     "https://json-schema.org/draft/2020-12/schema",
+		"$id":         "https://github.com/vultisig/recipes/jsonschema/asset",
+		"title":       "Asset",
+		"description": "An asset schema representing a blockchain asset with token, chain, and address information",
+		"type":        "object",
+		"properties": map[string]interface{}{
+			"token": map[string]interface{}{
+				"type":        "string",
+				"description": "Smart-contract address. Empty for native asset",
+			},
+			"chain": map[string]interface{}{
+				"type":        "string",
+				"description": "Blockchain network identifier as common.Chain string representation",
+			},
+			"address": map[string]interface{}{
+				"type":        "string",
+				"description": "User address on the blockchain",
+			},
+		},
+		"required":             []string{"chain", "address"},
+		"additionalProperties": false,
+	}
+}
