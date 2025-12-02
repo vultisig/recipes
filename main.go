a0b7368895f1270eab83c69a99ac32c758dd23d6
Raghav Sood <r@raghavsood.com>
2025-12-02 15:53:14 +0700

    recipes: add policy template definition

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate protoc -I./proto --go_opt=paths=source_relative --go_out=./types proto/constraint.proto proto/rule.proto proto/parameter_constraint.proto proto/resource.proto proto/policy.proto proto/recipe_specification.proto proto/thorchain.proto
+//go:generate protoc -I./proto --go_opt=paths=source_relative --go_out=./types proto/constraint.proto proto/rule.proto proto/parameter_constraint.proto proto/resource.proto proto/policy.proto proto/recipe_specification.proto proto/recipe_template.proto proto/thorchain.proto
 func main() {
 
 }
